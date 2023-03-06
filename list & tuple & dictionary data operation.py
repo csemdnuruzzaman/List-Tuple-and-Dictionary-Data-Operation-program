6f9c8850eecec2f93c2bda82a0711fb2bb11ae2a
csemdnuruzzaman <nuruzzamanpolytechnic@gmail.com>
2023-03-06 16:34:49 +0600

    first commit

---
+++ list & tuple & dictionary data operation.py
@@ -0,0 +1,19 @@
+list = ["Nuruzzaman",435296,"Agardari","Satkhira",3.15,"polytechnic"]
+print(list)
+print(list[0])
+print(list[2:])
+print(list[:3])
+tuple = ("Nuruzzaman",435296,"Agardari","Satkhira",3.15,"polytechnic")
+print(tuple)
+print(tuple[0])
+print(tuple[2:])
+print(tuple[:3])
+dictionary = {"Name":"Nuruzzaman","Roll":435296,"Technology":"Computer","shift":"1st","Session":1920}
+print(dictionary)
+print(dictionary["Name"])
+dictionary["Name"]="Abdur Rahmman"
+print(dictionary["Name"])
+print(dictionary["Roll"])
+print(dictionary["Technology"])
+print(dictionary["Session"])
+print(dictionary)
